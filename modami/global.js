37d69d86aa64a25e26821d3cb5d72781461d1268
itkddms <itkddms@naver.com>
2025-10-24 01:50:17 +0900

    docs

--- modami/global.js
+++
@@ -1,7 +0,0 @@
-// global.js
-window.APP_MODE = {
-  MOCK: false, // 여기를 true로 하면 테스트모드, false면 실제모드
-  TTS: true, // true면 TTS만 활성화
-  GPT: false, // true면 GPT만 활성화
-  IMAGE: true // 🆕 DALL·E (이미지 생성) 호출 허용 여부
-};
